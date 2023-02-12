6a1eba5f4f41ceb9c0bec073a6a2a2d778959439
Cristian Guzman <mono-717@hotmail.com>
2023-02-11 20:21:46 -0500

    v5

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,8 @@ let cont=true; // nos sirve para saber si el usuario ha elegido alguna opciòn v
 let vidasPC=3;
 let vidasJug=3;
 // const mokepones=["charizar","blastoise","sceptile","picachu","arcanine","golduck"]; //mokepones totalespara elegir
-const elementos={"charizar":"Fuego🔥", "blastoise":"Agua💧","sceptile":"Planta 🌱","picachu":"Rayo✨","arcanine":"Fuego🔥 + Planta🌱","golduck":"Agua💧 + Rayo✨" } //elementos de cada mokepon
-const ataquess=["Fuego🔥","Agua💧","Planta🌱","Rayo✨"]; // ataques existentes
-
+let ataquess=["Fuego🔥","Agua💧","Planta🌱","Rayo✨"]; // ataques existentes de todos los mokepones
+let ataquesTotales;
 let sectionMascota=document.getElementById("seleccionar-mascota");
 
 let sectionAtaques=document.getElementById("seleccionar-ataque");
@@ -42,9 +41,13 @@ let mostrarMokeponPc=document.getElementById("imagen-pc");//DIV de  mokepones el
 let mokepons=[];//aqui se almacenan los mokepones totales para elegir
 let contenedorTarjetas=document.getElementById("contenedor-tarjetas")
 let sectionContenedores=document.getElementById("contenedores");
-let ataquesActualJug;
-let ataquesActualPc;
-
+let ataquesJug;
+let ataquesPc;
+let botones={};
+let secuenciaAtaqueJug=[];  //secuencia Jug
+let secuenciaAtaquePc=[];  //secuencia Pc
+let contadorPuntosJug=0;    //para el combate
+let contadorPuntosPc=0;
 
 class Mokepon{    
     constructor(nombre,foto,vida){
@@ -55,8 +58,9 @@ class Mokepon{
     }
 }
 
+
 let charizar=new Mokepon("charizar",`./assets/charizar.png`,5);
-let blastoise=new Mokepon("blastoise",`./assets/blastoise.png`,5);
+let blastoise=new Mokepon("blastoise",`./assets/blastoise2.png`,5);
 let sceptile=new Mokepon("sceptile",`./assets/sceptile.png`,5);
 let picachu=new Mokepon("picachu",`./assets/picachu.png`,5);
 let arcanine=new Mokepon("arcanine",`./assets/arcanine.png`,5);
@@ -67,7 +71,7 @@ charizar.ataques.push(
     {nombre:`🔥`, id:"boton-fuego"},
     {nombre:`🔥`, id:"boton-fuego"},
     {nombre:`🔥`, id:"boton-fuego"},
-    {nombre:`🌱`, id:"boton-tierra"}
+    {nombre:`🌱`, id:"boton-planta"}
 )
 
 blastoise.ataques.push(
@@ -75,14 +79,14 @@ blastoise.ataques.push(
     {nombre:`💧`, id:"boton-agua"},
     {nombre:`💧`, id:"boton-agua"},
     {nombre:`💧`, id:"boton-agua"},
-    {nombre:`🌱`, id:"boton-tierra"}
+    {nombre:`🌱`, id:"boton-planta"}
 )
 
 sceptile.ataques.push(
-    {nombre:`🌱`, id:"boton-tierra"},
-    {nombre:`🌱`, id:"boton-tierra"},
-    {nombre:`🌱`, id:"boton-tierra"},
-    {nombre:`🌱`, id:"boton-tierra"},
+    {nombre:`🌱`, id:"boton-planta"},
+    {nombre:`🌱`, id:"boton-planta"},
+    {nombre:`🌱`, id:"boton-planta"},
+    {nombre:`🌱`, id:"boton-planta"},
     {nombre:`✨`, id:"boton-rayo"}
 )
 
@@ -91,15 +95,15 @@ picachu.ataques.push(
     {nombre:`✨`, id:"boton-rayo"},
     {nombre:`✨`, id:"boton-rayo"},
     {nombre:`✨`, id:"boton-rayo"},
-    {nombre:`🌱`, id:"boton-tierra"}
+    {nombre:`🌱`, id:"boton-planta"}
 )
 
 arcanine.ataques.push(
     {nombre:`🔥`, id:"boton-fuego"},
     {nombre:`🔥`, id:"boton-fuego"},
     {nombre:`🔥`, id:"boton-fuego"},
-    {nombre:`🌱`, id:"boton-tierra"},
-    {nombre:`🌱`, id:"boton-tierra"}
+    {nombre:`🌱`, id:"boton-planta"},
+    {nombre:`🌱`, id:"boton-planta"}
 )
 
 golduck.ataques.push(
@@ -111,6 +115,17 @@ golduck.ataques.push(
 )
 
 mokepons.push(charizar,blastoise,sceptile,picachu,arcanine,golduck)
+
+function extraerAtaquesTot(){ // para tener en un array los ataques totales del juego, nos servcira para hacer el cotejo en la funcion combate
+    let ataquesDuplicados=[] // aqui se guardan todos los ataques, pero habrà mas de uno del mismo nombre
+    for(x=0; x<mokepons.length; x++){
+        for(i=0; i<5; i++){
+        ataquesDuplicados.push(mokepons[x].ataques[i].id.split("boton-").slice(1).join()) // lo ultimo es para quitar el "boton-" del nombre que queremos guardar
+        }
+    }
+    let comodin=new Set(ataquesDuplicados); // convertimos creamos un objeto de ataques sin duplicados
+    ataquesTotales=[...comodin] //ahora convertimos en array nuestro conjunto sin duplicados
+}
 
 inciarJuego();
 
@@ -141,74 +156,128 @@ function selectMokeponPc(){
     imagenMokeponPcVs(mokeponPc); //cargamos las imagenes del mokepon elegido por la PC Vs
 
     spanMokePc.innerHTML=mokeponPc; // modificamos el DOM de la eleccion del pc
-    extraerAtaques();
+    extraerAtaquesJug();
+    extraerAtaquesPc();
+    secuenciaAtaque();
 }
 
-function atackFuego(){ //ataques jugador
-  ataqueActualJug=ataquess[0];
-   atackPc();   }
+let idAtaquesJug=[] // guardo los id (id de los botones) de los ataques del mokepone elegido por el jugador
+let arrayAtaquesPc=[] // guardo los id (nombres) de los ataques del mokepone elegido por el pc
 
-function atackAgua(){
-    ataqueActualJug=ataquess[1];
-     atackPc(); 
- }
-
-function atackTierra(){
-     ataqueActualJug=ataquess[2]; 
-     atackPc();
- }
-
- function atackRayo(){
-    ataqueActualJug=ataquess[3]; 
-     atackPc();
- }
-
-function atackPc(){ //ataque aleatorio del PC
-    ataqueActualPc=ataquess[aleatorio(0,ataquess.length-1)];
-    combate();
-}
-
-let guardarIdAtaques=[] // guardo los id de los ataques del mokepone elegido
-
-function extraerAtaques(){ // para guardar en la variable ataquesActualJug los ataques que corresponden al mokepon elegido por el jug y pc y mostrar en HTML los botones de ataque
+function extraerAtaquesJug(){ // para guardar en la variable ataquesJug los ataques que corresponden al mokepon elegido por el jug  y mostrar en HTML los botones de ataque
     for(e of mokepons ){
-        if(mokeponjug==e.nombre) ataquesActualJug=e.ataques;
-        if(mokeponPc==e.nombre) ataquesActualPc=e.ataques;
+        if(mokeponjug==e.nombre) ataquesJug=e.ataques;        
     }
     
-    ataquesActualJug.forEach(ataque=> {
-        let opcionesDeAtaques=`<button class="botones-ataque" id=${ataque.id}>${ataque.nombre}</button>`;
+    ataquesJug.forEach(ataque=> {
+        let opcionesDeAtaques=`<button class="botones-ataque BDeataque" id=${ataque.id}>${ataque.nombre}</button>`;
         let botonesDeAtaques=document.getElementById("botones-de-ataque");
         botonesDeAtaques.innerHTML+=opcionesDeAtaques;  
-        guardarIdAtaques.push(ataque.id)
+        idAtaquesJug.push(ataque.id)
     });
     
+    botones=document.querySelectorAll(".BDeataque");
     // cuando hacen click en los botones de ataque 
-    if(guardarIdAtaques.indexOf("boton-fuego")>=0) { // si nuestro mokepon tiene este ataque enntra el If
+    if(idAtaquesJug.indexOf("boton-fuego")>=0) { // si nuestro mokepon tiene este ataque enntra el If
         botonFuego=document.getElementById("boton-fuego");  
-        botonFuego.addEventListener("click", atackFuego );
+        // botonFuego.addEventListener("click", atackFuego );
     }
 
-    if(guardarIdAtaques.indexOf("boton-agua")>=0){
-        let botonAgua=document.getElementById("boton-agua");
-        botonAgua.addEventListener("click", atackAgua);
+    if(idAtaquesJug.indexOf("boton-agua")>=0){
+        botonAgua=document.getElementById("boton-agua");
+        // botonAgua.addEventListener("click", atackAgua);
     }
 
-    if(guardarIdAtaques.indexOf("boton-tierra")>=0){
-        let botonTierra=document.getElementById("boton-tierra");
-        botonTierra.addEventListener("click",atackTierra );
+    if(idAtaquesJug.indexOf("boton-tierra")>=0){
+        botonTierra=document.getElementById("boton-tierra");
+        // botonTierra.addEventListener("click",atackTierra );
     }
 
-    if(guardarIdAtaques.indexOf("boton-rayo")>=0){
-        let botonRayo=document.getElementById("boton-rayo")
-        botonRayo.addEventListener("click", atackRayo);
+    if(idAtaquesJug.indexOf("boton-rayo")>=0){
+        botonRayo=document.getElementById("boton-rayo")
+        // botonRayo.addEventListener("click", atackRayo);
     }
 }
-     
-function combate(){    
-    if(ataqueActualJug==ataquess[0] && ataqueActualPc==ataquess[1] || ataqueActualJug==ataquess[1] && ataqueActualPc==ataquess[2] || ataqueActualJug==ataquess[3] && ataqueActualPc==ataquess[2] || ataqueActualJug==ataquess[1] && ataqueActualPc==ataquess[3]|| ataqueActualJug==ataquess[2] && ataqueActualPc==ataquess[0]){
-        resulParcial="Tu pierdes😢";
-        
+   
+function extraerAtaquesPc(){ // para guardar en la variable ataquesPc los ataques que corresponden al mokepon elegido por el pc
+    for(e of mokepons ){
+        if(mokeponPc==e.nombre) { 
+        ataquesPc=e.ataques;         
+    }      
+    }
+    
+    for(ataque of ataquesPc){ //guardo en la variable los nombres de los ataques del mokepon del pc
+        arrayAtaquesPc.push(ataque.id.split("boton-").slice(1).join()); // guardamos solo el nombre del ataque, quitando el texto "boton-" ya que el id viene "boton-fuego" por ejemplo.
+
+    }
+}
+
+function secuenciaAtaque(){ //secuencia de ataques del jugador
+    secuenciaDeAtaquePc();
+    let contadorClicks=0; // cuando llega a 5 significa que ya uso los 5 botones y podemos invocar el combate
+    botones.forEach((boton) => {
+        boton.addEventListener("click", (e) => {
+            if(e.target.textContent=="🔥"){
+                secuenciaAtaqueJug.push("fuego")
+                boton.style.background="#1D3E65";
+                boton.disabled=true;                
+                contadorClicks++;               
+            } else if(e.target.textContent=="💧"){
+                secuenciaAtaqueJug.push("agua")
+                boton.style.background="#1D3E65"; 
+                boton.disabled=true;
+                contadorClicks++;
+            } else if(e.target.textContent=="🌱"){
+                secuenciaAtaqueJug.push("planta")
+                boton.style.background="#1D3E65";
+                boton.disabled=true;
+                contadorClicks++; 
+            }else if(e.target.textContent=="✨"){
+                secuenciaAtaqueJug.push("rayo")
+                boton.style.background="#1D3E65";
+                boton.disabled=true;
+                contadorClicks++; 
+            }
+            if(contadorClicks==5)  combate();   
+        })
+    });
+}
+
+function secuenciaDeAtaquePc(){ //secuencia de ataques aleatorio del PC
+    let n=arrayAtaquesPc.length; // guardamos el numero de ataques maximos del pc
+    let indiceAleatorio;
+    for(i=0; i<n; i++){
+        indiceAleatorio=aleatorio(0,arrayAtaquesPc.length-1);
+        secuenciaAtaquePc.push(arrayAtaquesPc[indiceAleatorio]); // pusheamos el arreglo de la secuencia del pc
+        arrayAtaquesPc.splice(indiceAleatorio,1) //eliminamos del array el valor del indice aleatorio escogido, para que no pueda ser elegida de nuevo
+    }    
+    console.log(secuenciaAtaquePc)
+    
+}
+
+function combate(){ 
+    // 1 pto para el que gane cada duelo 
+
+    for(i=0; i<secuenciaAtaqueJug.length; i++){
+        if(secuenciaAtaqueJug[i]==ataquesTotales[0] && secuenciaAtaquePc[i]==ataquesTotales[2] || secuenciaAtaqueJug[i]==ataquesTotales[2] && secuenciaAtaquePc[i]==ataquesTotales[1] || secuenciaAtaqueJug[i]==ataquesTotales[3] && secuenciaAtaquePc[i]==ataquesTotales[1] || secuenciaAtaqueJug[i]==ataquesTotales[2] && secuenciaAtaquePc[i]==ataquesTotales[3]|| secuenciaAtaqueJug[i]==ataquesTotales[1] && secuenciaAtaquePc[i]==ataquesTotales[0]){
+            console.log(`Tu pierdes😢, turno ${i+1}`);
+            contadorPuntosPc++;            
+            
+        } else if(secuenciaAtaqueJug[i]==secuenciaAtaquePc[i]|| secuenciaAtaqueJug[i]==ataquesTotales[0] && secuenciaAtaquePc[i]==ataquesTotales[3] || secuenciaAtaqueJug[i]==ataquesTotales[3] && secuenciaAtaquePc[i]==ataquesTotales[0] ) {
+            console.log(`Empate, turno ${i+1}`);
+            
+                       
+        } else if(secuenciaAtaqueJug[i]==ataquesTotales[2] && secuenciaAtaquePc[i]==ataquesTotales[0] || secuenciaAtaqueJug[i]==ataquesTotales[1] && secuenciaAtaquePc[i]==ataquesTotales[2] || secuenciaAtaqueJug[i]==ataquesTotales[1] && secuenciaAtaquePc[i]==ataquesTotales[3] || secuenciaAtaqueJug[i]==ataquesTotales[3] && secuenciaAtaquePc[i]==ataquesTotales[2]|| secuenciaAtaqueJug[i]==ataquesTotales[0] && secuenciaAtaquePc[i]==ataquesTotales[1]) {
+            console.log(`Tu ganas, turno ${i+1}`);
+            contadorPuntosJug++;
+        }
+    }  
+    contadorDepuntos(); 
+}
+
+function contadorDepuntos(){
+    if(contadorPuntosJug>contadorPuntosPc){
+        resulParcial=`Tu Ganas`;
         spanvidasJug.innerHTML=Number(spanvidasJug.innerText)-1;    
          //modifica el DOM con el nuevo numero de vidas del jug
         vidasJug=Number(spanvidasJug.innerText);
@@ -217,22 +286,19 @@ function combate(){
             crearMsjFinal(mensaje)        
       
         }
-      
-    } else if(ataqueActualJug==ataqueActualPc|| ataqueActualJug==ataquess[0] && ataqueActualPc==ataquess[3] || ataqueActualJug==ataquess[3] && ataqueActualPc==ataquess[0] ) {
-        resulParcial="Es un empate 📋"
-
-    } else if(ataqueActualJug==ataquess[1] && ataqueActualPc==ataquess[0] || ataqueActualJug==ataquess[2] && ataqueActualPc==ataquess[1] || ataqueActualJug==ataquess[2] && ataqueActualPc==ataquess[3] || ataqueActualJug==ataquess[3] && ataqueActualPc==ataquess[1]|| ataqueActualJug==ataquess[0] && ataqueActualPc==ataquess[2]) {
-        resulParcial="Tu ganas 🏆";
-
+    } else if(contadorPuntosJug==contadorPuntosPc){
+        resulParcial=`Es un empate`;
+        
+    } else {
+        resulParcial=`Tu pierdes`;
         spanvidasPc.innerHTML=Number(spanvidasPc.innerText)-1; //modifica el DOM con el nuevo numero de vidas del PC
         vidasPC=Number(spanvidasPc.innerText);
         if(vidasPC==0) {
             let mensaje=`TU ${mokeponjug} HA GANADO EL DUELO!!!🥳🎉🥳`
             crearMsjFinal(mensaje)                          
-        }           
-    }
+        }
+    }      
     crearMsj();
-   
 }
 
 function crearMsj(){   
@@ -255,10 +321,21 @@ function crearMsjFinal(resultado){
     sectionMsjFinal.appendChild(parrafo) 
 
     // deshabilitar botones de ataques
-    botonFuego.disabled=true;
-    botonAgua.disabled=true;
-    botonTierra.disabled=true;
-    botonRayo.disabled=true;
+    if(idAtaquesJug.indexOf("boton-fuego")>=0) { // si nuestro mokepon tiene este ataque enntra el If
+        botonFuego.disabled=true;        
+    }
+
+    if(idAtaquesJug.indexOf("boton-agua")>=0){
+        botonAgua.disabled=true;  
+    }    
+
+    if(idAtaquesJug.indexOf("boton-tierra")>=0){
+        botonTierra.disabled=true;        
+    }
+
+    if(idAtaquesJug.indexOf("boton-rayo")>=0){
+        botonRayo.disabled=true;       
+    }
 }
 
 function inciarJuego(){ //para desactivar inicialmente lo que no queremos que se muestre y cargar las tarjetas de mokepones en el HTML    
@@ -283,7 +360,8 @@ function inciarJuego(){ //para desactivar inicialmente lo que no queremos que se
             contenedorTarjetas.innerHTML+=opcionDeMokepones;     
         } else contenedorTarjetas.innerHTML+=opcionDeMokepones;
             
-    })     
+    }) 
+    extraerAtaquesTot()    
 }
 
 function reinciar(){
@@ -311,6 +389,9 @@ function imagenMokeponPcVs(mokepon){ //muestra imagen del mokepon elegido por el
         }
     } 
 }
+
+
+      
 
 
 
